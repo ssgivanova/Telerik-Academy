01233a3daa5676c24e79a75cd1ca2089c93c898b
syssboxx <syssboxx@gmail.com>
2016-04-17 04:13:03 +0300

    modift saddy kopper

--- C#1/07-Exams/03SaddyKopper/SaddyKopper.cs
+++ C#1/07-Exams/03SaddyKopper/SaddyKopper.cs
@@ -4,25 +4,23 @@ class SaddyKopper
     {
         static void Main()
         {
-            //int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
 
-            int n = 12345;
+            //int n = 12345;
             
-            bool hasMoreDigits = true;
             int value = 0;
             int sum = 0;
             int prod = 1;
             int countTransformation = 0;
             int result = 0;
 
-            //int value = n / (int)(Math.Pow(10,nbrDigits-1));
-            //Console.WriteLine(value);
-
             do
             {
+                //remove last digit
                 value = n / 10;
                 
                 //Console.WriteLine(n);
+                //find all digits at even position and find their sum
                 sum = 0;
                 string numStr = value.ToString();
                 for (int i = 0; i < numStr.Length; i+=2)
@@ -30,27 +28,55 @@ class SaddyKopper
                      sum += (int)Char.GetNumericValue(numStr[i]);
                 }
                 Console.WriteLine("n = {0} - sum {1}", value, sum);
-                prod *= sum;
-
-                //divide again
+                
+                //divide again to obtain new number with removed last digit 
                 n = value;
 
-                //Console.WriteLine("prod {0}",prod);
-                //if (prod / 10 == 0)
-                //{
-                    //hasMoreDigits = false;
-                    countTransformation++;
-                    result = prod;
-                //    break;
-                //}
-                //else
-                //{
-                //    n = prod;
-                //}
-            }
+
+                //get the product of all sums found
+                prod *= sum;
+                //product became new number end check if it has more than 1 digit
+                
+                
+            }//while no digits left
             while (n > 1);
+            countTransformation++;
+            Console.WriteLine("prod {0}", prod);
+
+            if (prod / 10 == 0)
+            {
+                result = prod;
+            }
+            else 
+            {
+                n = prod;
+                prod = 1;
+                while (n > 1)
+                {
+                    value = prod / 10;
+                    n = value;
+                    if (countTransformation >= 10) { break; }
+                    
+                }
+                result = prod;
+                countTransformation++;
+            }
+            
             Console.WriteLine("prod {0}", prod);
             Console.WriteLine("transformations {0}", countTransformation);
             Console.WriteLine("result {0}",result);
+
+            Console.WriteLine();
+
+            //print output
+            if (countTransformation < 10)
+            {
+                Console.WriteLine("{0}\n{1}", countTransformation, result);
+            }
+            else
+            {
+                Console.WriteLine(result);
+            }
+            
         }
     }
