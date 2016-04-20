60df1d47cb88e3fae4a3568a414d65a0e445a2c0
ssgivanova <ssgivanova@gmail.com>
2016-04-21 01:30:57 +0300

    add coffee machine and garden exam problems

---
+++ C#1/07-Exams/01CoffeeMachine/CoffeeMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+
+
+class CoffeeMachine
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+            int n5 = int.Parse(Console.ReadLine());
+            decimal amount = decimal.Parse(Console.ReadLine());
+            decimal price = decimal.Parse(Console.ReadLine());
+
+            decimal totalMoney = n1 * 0.05m + n2 * 0.10m + n3 * 0.20m + n4 * 0.50m + n5 * 1.00m;
+            decimal change = amount - price;
+            decimal left = totalMoney - change;
+
+            if (amount < price)
+            {
+                Console.WriteLine("More {0:f2}", price - amount);
+            }
+            else //amount >= price
+            {
+                if (totalMoney >= change)
+                {
+                    Console.WriteLine("Yes {0:f2}",left);
+                }
+                else // (totalMoney < change)
+                {
+                    Console.WriteLine("No {0:f2}",Math.Abs(left));
+                }
+            }
+        }
+    }
---
+++ C#1/07-Exams/01Garden/Garden.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+
+class Garden
+{
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            int tomatoSeedAmount = int.Parse(Console.ReadLine());
+            int tomatoArea = int.Parse(Console.ReadLine()); 
+            int cucumberSeedAmount = int.Parse(Console.ReadLine());
+            int cucumberArea = int.Parse(Console.ReadLine());
+            int potatoSeedAmount = int.Parse(Console.ReadLine());
+            int potatoArea = int.Parse(Console.ReadLine());
+            int carotSeedAmount = int.Parse(Console.ReadLine());
+            int carotArea = int.Parse(Console.ReadLine());
+            int cabaggeSeedAmount = int.Parse(Console.ReadLine());
+            int cabaggeArea = int.Parse(Console.ReadLine());
+            int beansSeedAmount = int.Parse(Console.ReadLine());
+
+            int totalArea = 250;
+            decimal totalCost = 0.5m * tomatoSeedAmount + 0.4m * cucumberSeedAmount + 0.25m * potatoSeedAmount
+                + 0.6m * carotSeedAmount + 0.3m * cabaggeSeedAmount + 0.4m * beansSeedAmount;
+            int othersArea = tomatoArea + cucumberArea + potatoArea + carotArea + cabaggeArea;
+
+            Console.WriteLine("Total costs: {0:f2}",totalCost);
+
+            if (othersArea > totalArea)
+            {
+                Console.WriteLine("Insufficient area");
+            }
+            else
+            {
+                int beansArea = totalArea - othersArea;
+                if (beansArea <= 0)
+                {
+                    Console.WriteLine("No area for beans");
+                }
+                else
+                {
+                    Console.WriteLine("Beans area: {0}", beansArea);
+                }
+            }
+        }
+}
