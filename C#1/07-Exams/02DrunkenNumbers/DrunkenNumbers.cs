53e69ef5ca11464d7935e3c629d865036cceb1c4
ssgivanova <ssgivanova@gmail.com>
2016-04-24 12:21:45 +0300

    add secrets and drunken numbers problems

---
+++ C#1/07-Exams/02DrunkenNumbers/DrunkenNumbers.cs
@@ -0,0 +1,149 @@
+﻿using System;
+class DrunkenNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sumMitko = 0; //left digits
+        int sumVladko = 0; //right digits
+        int totalSum = 0;
+        int difference = 0;
+
+        char winner = '\0';
+        int max = 0;
+
+        // FIRST SOLUTION - WITH STRINGS
+
+        for (int i = 0; i < n; i++)
+        {
+            //if the num starts with 0 - remove the leading zeros when parsing it to int
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                number = Math.Abs(number);
+            }
+
+            string num = number.ToString();
+            
+            int digit = 0;
+
+            int middleDigit = num[num.Length / 2] - '0';
+
+            //even number digits
+            //left digits - mitko
+            for (int k = 0; k < num.Length / 2; k++)
+            {
+                digit = num[k] - '0';
+                sumMitko += digit;
+            }
+
+            //right digits - vladko
+            for (int k = num.Length / 2; k < num.Length; k++)
+            {
+                digit = num[k] - '0';
+                sumVladko += digit;
+            }
+            // odd number digits
+            if (num.Length % 2 != 0)
+            {
+                sumMitko += middleDigit;
+            }
+            
+            //Console.WriteLine("mitko - sum {0}",sumMitko);
+            //Console.WriteLine("vladko - sum {0}", sumVladko);
+
+            difference = Math.Abs(sumVladko - sumMitko);
+            totalSum = sumMitko + sumVladko;
+        }
+        if (sumMitko == sumVladko)
+        {
+            Console.WriteLine("No {0}", totalSum);
+        }
+        else
+        {
+            if (sumMitko > max)
+            {
+                max = sumMitko; winner = 'M';
+            }
+            if (sumVladko > max)
+            {
+                max = sumVladko; winner = 'V';
+            }
+            Console.WriteLine("{0} {1}", winner, difference);
+        }
+
+        /* SECOND SOLUTION - WITH REMAINDER OF DIVISION
+
+        int n = int.Parse(Console.ReadLine());
+
+        // Vladko beers
+        int rightDigitsSum = 0;
+
+        // Mitko beers
+        int leftDigitsSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            // Read the number, skipping the leading zeroes
+            int currentNumber = int.Parse(Console.ReadLine());
+
+            // Turn negative numbers to positive (remove the "-" sign)
+            if (currentNumber < 0)
+            {
+                currentNumber *= -1;
+            }
+
+            // Calculate the total number of digits
+            int numberOfDigits = 0;
+            int num = currentNumber;
+            while (num != 0)
+            {
+                num = num / 10;
+                numberOfDigits++;
+            }
+
+            // Sum of digits at the right side
+            for (int right = 0; right < numberOfDigits / 2; right++)
+            {
+                int currentDigit = currentNumber % 10;
+                currentNumber = currentNumber / 10;
+                rightDigitsSum += currentDigit;
+            }
+
+            if (numberOfDigits % 2 == 1)
+            {
+                // We have a middle digit --> add it to both sums
+                int middleDigit = currentNumber % 10;
+                leftDigitsSum += middleDigit;
+                rightDigitsSum += middleDigit;
+                currentNumber = currentNumber / 10;
+            }
+
+            // Sum of digits at the left side
+            for (int left = 0; left < numberOfDigits / 2; left++)
+            {
+                int currentDigit = currentNumber % 10;
+                currentNumber = currentNumber / 10;
+                leftDigitsSum += currentDigit;
+            }
+        }
+
+        // Print the results: winner and difference
+        int difference = leftDigitsSum - rightDigitsSum;
+        if (difference > 0)
+        {
+            Console.WriteLine("M {0}", difference);
+        }
+        else if (difference < 0)
+        {
+            Console.WriteLine("V {0}", difference * -1);
+        }
+        else
+        {
+            Console.WriteLine("No {0}", rightDigitsSum + leftDigitsSum);
+        }
+         * */
+    }
+ }
---
+++ C#1/07-Exams/02Secrets/Secrets.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Numerics;
+class Secrets
+{
+   static void Main()
+   {
+       //because n can has <= 400 digits = 1.10^400 > long -> bigInteger
+       BigInteger n = BigInteger.Parse(Console.ReadLine());
+       BigInteger num = n;
+
+       if (n < 0)
+       {
+           n *= -1;
+       }
+
+       //find odd and even sum
+       //find special sum
+       //find last digit of special sum = length of alpha sequence
+       //find R = sum % 26;
+       //find letters of the alphabets
+       //find first letter of the sequence = R + 1 letter in alphabet
+       //fill all the Length netx letters in the sequence
+       //print special sum and sequence
+
+       int sumOdd = 0;
+       int sumEven = 0;
+       int sum = 0;
+       int position = 1;
+       int digit = 0;
+
+       //find number of digits
+       int countDigits = 0;
+       while (n > 0)
+       {
+           digit = (int)(n % 10);
+           n = n / 10;
+           countDigits++;
+           //odd and even position sums
+           if (position % 2 != 0)
+           {
+               sumOdd += digit * position * position;
+           }
+           else
+           {
+               sumEven += digit * digit * position;
+           }
+           position++;
+        }
+       sum = sumEven + sumOdd;
+
+       int r = sum % 26;
+       //length of the sequance = last digit of sum
+       int length = sum % 10;
+
+       //print letters
+       //Console.WriteLine((int)'A'); //65
+       //Console.WriteLine((int)'Z'); //90
+
+       string result = "";
+       int firstIndex = r + 1;
+
+       //get first letter = the letter of the alphabet at position R+1 
+       //char first = (char)(65 + firstIndex-1);
+       char first = (char)('A'+ firstIndex -1);
+       result += first;
+
+       //create the alpha sequence - get the next letters
+       char letter = first;
+       for (int i = 1; i < length; i++)
+       {
+           letter ++;
+           if (letter > 'Z') //'Z'=90
+           {
+               //"out of the alphabet" - first start again from A : remove the ascci code of Z (90)
+               //so the index of the next letter is 1, and then add again 'A'
+               letter = (char)(letter - 'Z');
+               letter = (char)(letter + 'A' - 1);
+           }
+           result += letter;
+       }
+       
+       //output
+       Console.WriteLine(sum);
+       if (length == 0)
+       {
+           Console.WriteLine("{0} has no secret alpha-sequence", num);
+       }
+       else
+       {
+           Console.WriteLine(result);
+       }
+   }
+}
