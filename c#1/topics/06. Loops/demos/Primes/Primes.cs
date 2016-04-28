ed52b701ccd2104f50f677acd7c637f1c2dbdac1
ssgivanova <ssgivanova@gmail.com>
2016-04-28 11:57:17 +0300

    c# topics added

---
+++ c#1/topics/06. Loops/demos/Primes/Primes.cs
@@ -0,0 +1,47 @@
+using System;
+
+class Primes
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        string numberAsString = Console.ReadLine();
+        int n = int.Parse(numberAsString);
+
+        Console.Write("m = ");
+        numberAsString = Console.ReadLine();
+        int m = int.Parse(numberAsString);
+
+        if (1 < n && n < m)
+        {
+            for (int num = n; num <= m; num++)
+            {
+                bool prime = true;
+                int divider = 2;
+                int maxDivider = (int) Math.Sqrt(num);
+
+                while (divider <= maxDivider)
+                {
+                    if (num % divider == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+
+                    divider++;
+                }
+
+                if (prime)
+                {
+                    Console.Write("{0} ", num);
+                }
+            }
+
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Invalid range!");
+        }
+    }
+}
